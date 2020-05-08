d1c7d3256c36b43708dcaa0506781960b12b2b92
kimjanghyean <mskjh6@naver.com>
2020-05-08 15:40:25 +0900

    deploy.sh 추가

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,7 +6,7 @@ echo "> $REPOSITORY 디렉터리로 이동"
 cd $REPOSITORY
 
 echo "> 배포 파일 복사"
-cp $REPOSITORY/zip/build/libs/*.war $REPOSITORY/
+cp $REPOSITORY/zip/*.war $REPOSITORY/
 
 echo "> 구동 중인 애플리케이션 PID 검색"
 CURRENT_PID=$(pgrep -fl 'springboot*' | grep war | awk '{ print $1 }')
@@ -26,3 +26,4 @@ echo "> 새 애플리케이션($JAR_NAME) 배포"
 nohup java -jar \
 	-Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties -Dspring.profiles.active=real \
 	$REPOSITORY/$JAR_NAME 2>&1 &
+
